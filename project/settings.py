c6626d4e05948123fa827f3e620a8f5cb7cdc337
nadavc25 <nadavc25@gmail.comgit config --global user.name>
2023-11-26 20:40:34 +0200

    upgrade

--- project/settings.py
+++ project/settings.py
@@ -172,8 +172,8 @@ STATICFILES_DIRS = [
 
 
 
-MEDIA_ROOT = BASE_DIR / 'static/images'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+MEDIA_ROOT = BASE_DIR / '/static/images'
+STATIC_ROOT = BASE_DIR / '/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
